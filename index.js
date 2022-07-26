117f7ab153463e744a58422c32d13d2a2eedef35
Duy Pham <ericpham19@outlook.com>
2022-07-26 14:46:08 +1000

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+const dodger = document.getElementById('dodger');
+
+dodger.style.backgroundColor = 'blue';
+
+function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace ("px", "");
+       const left = parseInt(leftNumbers,10);
+    if (left > 0 ){
+        dodger.style.left = `${left - 2}px`;
+    }
+}
+
+function moveDodgerRight(){
+    const leftNumbers= dodger.style.left.replace ("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if (left < 360){
+        dodger.style.left= `${left + 2}px`;
+    }
+
+
+}
+document.addEventListener('keydown',function(event){
+    if (event.keyCode === 37){
+       moveDodgerLeft();
+    if (event.keyCode === 39){
+        moveDodgerRight();
+    }
+    }
+    
+});
+
+
